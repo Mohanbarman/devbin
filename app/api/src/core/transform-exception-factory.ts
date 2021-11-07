d58423b6451b54e60a33926e5c43962e4814de5a
Mohan Barman <mohanbarman9711@gmail.com>
2021-11-07 19:57:23 +0530

    ADD: custom function to parse validation errors

---
+++ app/api/src/core/transform-exception-factory.ts
@@ -0,0 +1,12 @@
+import { ValidationError } from '@nestjs/common';
+import { ValidationFailedException } from '../exceptions/validation-failed.exception';
+
+export const transformExceptionFactory = (errors: ValidationError[]) => {
+  const _errors: Record<string, string[]> = {};
+
+  for (const error of errors) {
+    _errors[error.property] = Object.values(error.constraints || {});
+  }
+
+  return new ValidationFailedException(_errors);
+};
