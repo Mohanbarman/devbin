8d04b6c0c074d51f872d5eb0959539ede34291ed
Mohan Barman <mohanbarman9711@gmail.com>
2021-11-07 20:00:12 +0530

    UPDATE: using custom param decorators

--- app/api/src/modules/auth/auth.controller.ts
+++ app/api/src/modules/auth/auth.controller.ts
@@ -1,20 +1,14 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Req,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
-import { Response, Request } from 'express';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { Response } from 'express';
 
-import { createResponse } from '../../utils';
+import { createResponse } from '../../core';
 import { UserTransformer } from '../../transformers/user.transformer';
 import { AuthService } from './auth.service';
 import { LoginDTO, RegisterDTO } from './dto';
 import { TSession } from '../../types';
 import { AuthGuard } from '../../guards/auth.guard';
+import { GetSession, GetUser } from '../../param-decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -23,23 +17,25 @@ export class AuthController {
   @Post('login')
   async login(
     @Body() data: LoginDTO,
-    @Req() req: Request,
     @Res() res: Response,
+    @GetSession() session: TSession,
   ) {
-    const user = await this.authService.login(data, req.session as TSession);
-    return createResponse(res, new UserTransformer(user));
+    const user = await this.authService.login(data, session);
+    const transformed = new UserTransformer(user);
+    return createResponse(res, await transformed.data);
   }
 
   @Post('register')
   async register(@Res() res: Response, @Body() data: RegisterDTO) {
     const user = await this.authService.register(data);
-    return createResponse(res, new UserTransformer(user));
+    const transformed = new UserTransformer(user);
+    return createResponse(res, await transformed.data);
   }
 
   @Get('me')
   @UseGuards(AuthGuard)
-  async me(@Req() req: Request, @Res() res: Response) {
-    const session = req.session as TSession;
-    return createResponse(res, new UserTransformer(session.user));
+  async me(@GetUser() user: User, @Res() res: Response) {
+    const transformed = new UserTransformer(user);
+    return createResponse(res, await transformed.data);
   }
 }
